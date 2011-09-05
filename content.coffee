25337e1599bc561608065258192692cb9e81e4d6
Howard Yeh <hayeah@gmail.com>
2011-09-05 17:11:14 +0800

    refactor. FakeLike class

--- content.coffee
+++ content.coffee
@@ -6,9 +6,44 @@ if !DEBUG
 else
   console = window.console
 
+p = (o) -> console.log(o)
 
+DOMAIN_RE = /^https?:\/\/(www\.)?(facebook.com|facebook.net|fbcdn.net|connect.facebook)/i
 
 SECRET = "" + Math.random()
+
+class FakeLike
+  constructor: (button,onclick) ->
+    @trigger = onclick
+    @button = button # thing to replace
+    @fake_button = this.create_fake_button()
+    @button.replaceWith(@fake_button)
+  create_fake_button: ->
+    button = $("<span class='faceOff-like'></span>")
+    imgURL = chrome.extension.getURL("faceoff-icon.png");
+    button.css
+     "background": "url(#{imgURL}) 0 0 no-repeat"
+     "height": "24px"
+     "width": "51px"
+     "display": "inline-block"
+    button.bind "mouseenter", () =>
+      button.css "background-position": "0 -24px"
+    button.bind "mouseleave", () =>
+      button.css "background-position": "0 0"
+    button.click (e) => @trigger(e)
+    return button
+  loading: (element) ->
+    this.show_spinner()
+    @fake_button.replaceWith($(element))
+    @fake_button = null
+  loaded: () ->
+    @spinner.remove()
+    @spinner = null
+  show_spinner: ->
+    @spinner = $("<img/>")
+    imgURL = chrome.extension.getURL("loader.gif")
+    @spinner.attr("src",imgURL)
+    @fake_button.before(@spinner)
 
 class LikeButtonShadow
   constructor: (iframe) ->
@@ -54,8 +89,29 @@ class LikeButtonShadow
     @spinner.remove()
     @spinner = null
 
+class FBLikeTag
+  constructor: (tag) ->
+    @tag = $(tag)
+    @fake = new FakeLike(@tag, => @turn_on())
+  turn_on: ->
+    setTimeout \
+      () => @display(),
+      2000
+  display: ->
+    @fake.loaded $("<div>Great Success</div>")
 
-
+class XFBML
+  constructor: (script) ->
+    @script = script
+    $(document).ready () =>
+      this.prevent_loading()
+  prevent_loading: () ->
+    this.replace_likes()
+  replace_likes: () ->
+    likes = $("fb\\:like")
+    console.log likes
+    for tag in likes
+      new FBLikeTag(tag)
 
 like_button_beforeload = (event) ->
   iframe = event.target
@@ -67,15 +123,29 @@ like_button_beforeload = (event) ->
     event.preventDefault()
     return false
 
+sdk_beforeload = (event) ->
+  script = event.target
+  new XFBML(script)
+  event.preventDefault()
+  return false
+
 facebook_off = (event) ->
   if md = event.url.match(DOMAIN_RE)
-    if event.target.constructor == HTMLIFrameElement
+    type = event.target.constructor
+    if type == HTMLIFrameElement
       # replace if it's the like button iframe
       # TODO detect URL for like.php
-      console.log("like:", event.url)
+      console.log("block iframe:", event.url)
       like_button_beforeload(event)
+    else if type == HTMLScriptElement
+      console.log("block script:", event.url)
+      sdk_beforeload(event)
     else
-      # block everything else
+      console.log("block:", event.url)
+      # block everything else from facebook
       event.preventDefault()
+  else
+    # event.preventDefault()
+    console.log("load: "+event.url)
 
 document.addEventListener "beforeload", facebook_off, true
--- content.js
+++ content.js
@@ -1,8 +1,65 @@
 (function() {
-  var DOMAIN_RE, LikeButtonShadow, SECRET, facebook_off, like_button_beforeload;
+  var DEBUG, DOMAIN_RE, FBLikeTag, FakeLike, LikeButtonShadow, SECRET, XFBML, console, facebook_off, like_button_beforeload, p, sdk_beforeload;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  DOMAIN_RE = /^https?:\/\/(www\.)?(facebook.com|facebook.net|fbcdn.net)/i;
+  DEBUG = true;
+  if (!DEBUG) {
+    console = {};
+    console.log = function() {};
+  } else {
+    console = window.console;
+  }
+  p = function(o) {
+    return console.log(o);
+  };
+  DOMAIN_RE = /^https?:\/\/(www\.)?(facebook.com|facebook.net|fbcdn.net|connect.facebook)/i;
   SECRET = "" + Math.random();
+  FakeLike = (function() {
+    function FakeLike(button, onclick) {
+      this.trigger = onclick;
+      this.button = button;
+      this.create_fake_button();
+      this.button.replaceWith(this.fake_button);
+    }
+    FakeLike.prototype.create_fake_button = function() {
+      var imgURL;
+      this.fake_button = $("<span class='faceOff-like'></span>");
+      imgURL = chrome.extension.getURL("faceoff-icon.png");
+      this.fake_button.css({
+        "background": "url(" + imgURL + ") 0 0 no-repeat",
+        "height": "24px",
+        "width": "51px",
+        "display": "inline-block"
+      });
+      this.fake_button.bind("mouseenter", __bind(function() {
+        return this.fake_button.css({
+          "background-position": "0 -24px"
+        });
+      }, this));
+      this.fake_button.bind("mouseleave", __bind(function() {
+        return this.fake_button.css({
+          "background-position": "0 0"
+        });
+      }, this));
+      this.fake_button.click(__bind(function(e) {
+        this.loading();
+        return this.trigger(e);
+      }, this));
+      return this.fake_button;
+    };
+    FakeLike.prototype.loading = function() {
+      var imgURL;
+      this.spinner = $("<img/>");
+      imgURL = chrome.extension.getURL("loader.gif");
+      this.spinner.attr("src", imgURL);
+      this.fake_button.replaceWith(this.spinner);
+      return this.fake_button = null;
+    };
+    FakeLike.prototype.loaded = function(element) {
+      this.spinner.replaceWith(element);
+      return this.spinner = null;
+    };
+    return FakeLike;
+  })();
   LikeButtonShadow = (function() {
     function LikeButtonShadow(iframe) {
       iframe.shadow = this;
@@ -64,6 +121,46 @@
     };
     return LikeButtonShadow;
   })();
+  FBLikeTag = (function() {
+    function FBLikeTag(tag) {
+      this.tag = $(tag);
+      this.fake = new FakeLike(this.tag, __bind(function() {
+        return this.turn_on();
+      }, this));
+    }
+    FBLikeTag.prototype.turn_on = function() {
+      return setTimeout(__bind(function() {
+        return this.display();
+      }, this), 2000);
+    };
+    FBLikeTag.prototype.display = function() {
+      return this.fake.loaded($("<div>Great Success</div>"));
+    };
+    return FBLikeTag;
+  })();
+  XFBML = (function() {
+    function XFBML(script) {
+      this.script = script;
+      $(document).ready(__bind(function() {
+        return this.prevent_loading();
+      }, this));
+    }
+    XFBML.prototype.prevent_loading = function() {
+      return this.replace_likes();
+    };
+    XFBML.prototype.replace_likes = function() {
+      var likes, tag, _i, _len, _results;
+      likes = $("fb\\:like");
+      console.log(likes);
+      _results = [];
+      for (_i = 0, _len = likes.length; _i < _len; _i++) {
+        tag = likes[_i];
+        _results.push(new FBLikeTag(tag));
+      }
+      return _results;
+    };
+    return XFBML;
+  })();
   like_button_beforeload = function(event) {
     var iframe;
     iframe = event.target;
@@ -75,15 +172,29 @@
       return false;
     }
   };
+  sdk_beforeload = function(event) {
+    var script;
+    script = event.target;
+    new XFBML(script);
+    event.preventDefault();
+    return false;
+  };
   facebook_off = function(event) {
-    var md;
+    var md, type;
     if (md = event.url.match(DOMAIN_RE)) {
-      if (event.target.constructor === HTMLIFrameElement) {
-        console.log("like:", event.url);
+      type = event.target.constructor;
+      if (type === HTMLIFrameElement) {
+        console.log("block iframe:", event.url);
         return like_button_beforeload(event);
+      } else if (type === HTMLScriptElement) {
+        console.log("block script:", event.url);
+        return sdk_beforeload(event);
       } else {
+        console.log("block:", event.url);
         return event.preventDefault();
       }
+    } else {
+      return console.log("load: " + event.url);
     }
   };
   document.addEventListener("beforeload", facebook_off, true);
